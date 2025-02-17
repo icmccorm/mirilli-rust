57ac437cb3512493172660c256866e34e8551d03
Ian McCormack <icmccorm@andrew.cmu.edu>
2025-02-17 16:16:57 +0000

    Added step to merge static archives for Linux builds. bsanrt -> bsan-rt.

--- src/bootstrap/src/core/build_steps/bsan.rs
+++ src/bootstrap/src/core/build_steps/bsan.rs
@@ -1,6 +1,5 @@
 use std::path::PathBuf;
 
-use super::llvm;
 use crate::core::build_steps::compile;
 use crate::core::build_steps::compile::{apple_darwin_sign_file, apple_darwin_update_library_name};
 use crate::core::build_steps::llvm::{
@@ -10,11 +9,8 @@ use crate::core::build_steps::llvm::{
 use crate::core::build_steps::tool::{SourceType, prepare_tool_cargo};
 use crate::core::builder::{Builder, RunConfig, ShouldRun, Step};
 use crate::core::config::TargetSelection;
-use crate::utils::helpers::dylib;
-use crate::{Compiler, Kind, Mode, Path, fs, helpers, t};
-
-const BSAN_CORE_PATH: &str = "src/tools/bsan/bsanrt";
-const BSAN_RT_DYLIB: &str = "bsanrt";
+use crate::helpers::is_dylib;
+use crate::*;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct BsanRT {
@@ -27,13 +23,13 @@ impl Step for BsanRT {
     const ONLY_HOSTS: bool = true;
     fn should_run(run: ShouldRun<'_>) -> ShouldRun<'_> {
         let builder = run.builder;
-        run.path(BSAN_CORE_PATH).default_condition(
+        run.path("src/tools/bsan/bsan-rt").default_condition(
             builder.config.extended
                 && builder.config.tools.as_ref().map_or(
                     builder.build.unstable_features(),
                     |tools| {
                         tools.iter().any(|tool: &String| match tool.as_str() {
-                            x => BSAN_RT_DYLIB == x,
+                            x => "bsan-rt" == x,
                         })
                     },
                 ),
@@ -52,14 +48,7 @@ impl Step for BsanRT {
         let target = self.target;
         let compiler = self.compiler;
 
-        builder.ensure(llvm::Llvm { target });
-        builder.ensure(BsanRTCore { compiler, target });
-
-        let compiler_rt_dir = builder.src.join("src/llvm-project/compiler-rt");
-        if !compiler_rt_dir.exists() {
-            return None;
-        }
-
+        let bindir = builder.sysroot_target_bindir(compiler, target);
         let out_dir = builder.native_dir(self.target).join("sanitizers");
 
         let runtime = supports_bsan(&out_dir, self.target, &builder.config.channel);
@@ -69,9 +58,19 @@ impl Step for BsanRT {
         let runtime = runtime.unwrap();
 
         let LlvmResult { llvm_config, .. } = builder.ensure(Llvm { target: builder.config.build });
+
+        let compiler_rt_dir = builder.src.join("src/llvm-project/compiler-rt");
+        if !compiler_rt_dir.exists() {
+            return None;
+        }
+
+        let core_runtime_path = builder.ensure(BsanRTCore { compiler, target });
+        let core_runtime_parent_dir = core_runtime_path.parent().unwrap();
+
         if builder.config.dry_run() {
             return Some(runtime);
         }
+
         let _guard = builder.msg_unstaged(Kind::Build, "bsan", self.target);
         let _time = helpers::timeit(builder);
 
@@ -87,6 +86,7 @@ impl Step for BsanRT {
         cfg.define("COMPILER_RT_DEFAULT_TARGET_ONLY", "ON");
         cfg.define("COMPILER_RT_USE_LIBCXX", "OFF");
         cfg.define("LLVM_CONFIG_PATH", &llvm_config);
+
         // On Darwin targets the sanitizer runtimes are build as universal binaries.
         // Unfortunately sccache currently lacks support to build them successfully.
         // Disable compiler launcher on Darwin targets to avoid potential issues.
@@ -98,10 +98,8 @@ impl Step for BsanRT {
         let suppressed_compiler_flag_prefixes: &[&str] =
             if self.target.contains("apple-darwin") { &["-mmacosx-version-min="] } else { &[] };
 
-        let sysroot = &builder.sysroot_target_libdir(self.compiler, self.target);
-        let sysroot = sysroot.display();
         let mut ldflags = LdFlags::default();
-        ldflags.push_all(format!("-L{sysroot}"));
+        ldflags.push_all(format!("-L{}", &core_runtime_parent_dir.display()));
 
         configure_cmake(
             builder,
@@ -118,15 +116,19 @@ impl Step for BsanRT {
         cfg.build_target(&runtime.cmake_target);
         cfg.build();
 
+        if !is_dylib(&runtime.name) {
+            let llvm_ar_bin = bindir.join(exe("llvm-ar", compiler.host));
+            if let Err(_) = append_archive(builder, &llvm_ar_bin, &core_runtime_path, &runtime.path)
+            {
+                return None;
+            }
+        }
+
         let libdir = builder.sysroot_target_libdir(compiler, target);
         let dst = libdir.join(&runtime.name);
         builder.copy_link(&runtime.path, &dst);
 
-        if target == "x86_64-apple-darwin"
-            || target == "aarch64-apple-darwin"
-            || target == "aarch64-apple-ios"
-            || target == "aarch64-apple-ios-sim"
-            || target == "x86_64-apple-ios"
+        if target.contains("-apple-")
         {
             // Update the library’s install name to reflect that it has been renamed.
             apple_darwin_update_library_name(builder, &dst, &format!("@rpath/{}", runtime.name));
@@ -134,9 +136,39 @@ impl Step for BsanRT {
             // so we will sign it again.
             apple_darwin_sign_file(builder, &dst);
         }
-
         Some(runtime)
     }
+}
+
+fn append_archive(
+    builder: &Builder<'_>,
+    llvm_ar: &PathBuf,
+    source: &PathBuf,
+    destination: &PathBuf,
+) -> std::io::Result<()> {
+    let temp_dir = builder.build.tempdir().join("bsan-rt");
+    if fs::exists(&temp_dir)? {
+        fs::remove_dir_all(&temp_dir)?;
+    }
+    fs::create_dir_all(&temp_dir)?;
+
+    let _ = Command::new(&llvm_ar).current_dir(&temp_dir).arg("-x").arg(source).status()?;
+
+    let file_names: Vec<String> = fs::read_dir(&temp_dir)?
+        .filter_map(|entry| {
+            let path = entry.ok()?.path();
+            if path.is_file() { path.to_str().map(|s| s.to_owned()) } else { None }
+        })
+        .collect();
+
+    let _ = Command::new(llvm_ar)
+        .current_dir(&temp_dir)
+        .arg("-r")
+        .arg(destination)
+        .args(file_names)
+        .status()?;
+
+    Ok(())
 }
 
 pub fn supports_bsan(
@@ -165,7 +197,7 @@ pub struct BsanRTCore {
 impl Step for BsanRTCore {
     type Output = PathBuf;
     fn should_run(run: ShouldRun<'_>) -> ShouldRun<'_> {
-        run.path("src/tools/bsan/bsanrt")
+        run.never()
     }
 
     fn make_run(run: RunConfig<'_>) {
@@ -189,39 +221,16 @@ impl Step for BsanRTCore {
             mode,
             target,
             kind,
-            BSAN_CORE_PATH,
+            "src/tools/bsan/bsan-rt",
             SourceType::InTree,
             &Vec::new(),
         );
         cargo.env("BSAN_HEADER_DIR", builder.cargo_out(compiler, mode, target));
-
-        // we check this below
         let build_success = compile::stream_cargo(builder, cargo, vec![], &mut |_| {});
         if !build_success {
             crate::exit!(1);
         } else {
-            let file_name = dylib(BSAN_RT_DYLIB, target);
-            let runtime = builder.cargo_out(compiler, mode, target).join(&file_name);
-            if target == "x86_64-apple-darwin"
-                || target == "aarch64-apple-darwin"
-                || target == "aarch64-apple-ios"
-                || target == "aarch64-apple-ios-sim"
-                || target == "x86_64-apple-ios"
-            {
-                // Update the library’s install name to reflect that it has been renamed.
-                apple_darwin_update_library_name(
-                    builder,
-                    &runtime,
-                    &format!("@rpath/{}", file_name),
-                );
-                // Upon renaming the install name, the code signature of the file will invalidate,
-                // so we will sign it again.
-                apple_darwin_sign_file(builder, &runtime);
-            }
-            let libdir = builder.sysroot_target_libdir(compiler, target);
-            let dst = libdir.join(dylib(BSAN_RT_DYLIB, target));
-            builder.copy_link(&runtime, &dst);
-            dst
+            builder.cargo_out(compiler, mode, target).join("libbsan_rt.a")
         }
     }
 }
